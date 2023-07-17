d1d44a2b1d2f99b76cd5d6963433c461bc455390
ad230 <aleshadmi@mail.ru>
2023-07-18 00:42:49 +0300

    #3 Return negative

---
+++ 8 kyu/return_negative/src/ReturnNegative.java
@@ -0,0 +1,15 @@
+public class ReturnNegative {
+
+    public static int makeNegative(final int x) {
+        if (x > 0){
+            return -x;
+        } else if (x<0)
+        {
+            return x;
+        } else
+        {
+            return 0;
+        }
+    }
+
+}
