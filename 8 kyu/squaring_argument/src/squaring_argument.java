25d9a10ec36f3e2069fa5e4ac5890782df5dd665
ad230 <aleshadmi@mail.ru>
2023-07-18 11:39:17 +0300

    #4 Squaring argument. Second Solution.

--- 8 kyu/squaring_argument/src/squaring_argument.java
+++ 8 kyu/squaring_argument/src/squaring_argument.java
@@ -3,3 +3,12 @@ public class squaring_argument {
         return n*n;
     }
 }
+
+// 2 Solution
+/*
+public class squaring_argument {
+    public static int square(int n){
+        return (int)Math.pow(n, 2.0);
+    }
+}
+ */
