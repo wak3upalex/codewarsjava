3286bf688572b24257799f801621f792f20d26fb
ad230 <aleshadmi@mail.ru>
2023-10-04 00:34:45 +0300

    # 13 - Basic subclasses - Adam and Eve minor fix for declaration of Human array

--- 8 kyu/adam_and_eve/src/God.java
+++ 8 kyu/adam_and_eve/src/God.java
@@ -1,6 +1,6 @@
 public class God {
     public static Human[] create() {
-        Human human_array[] = new Human[1];
+        Human[] human_array = new Human[1];
         Man man1 = new Man();
         Woman woman1 = new Woman();
         human_array[0] = man1;
